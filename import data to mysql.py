9cf00e77157cb34d566b0d67a3751d8a56b6ac7e
Wijaksono-4 <73674801+Wijaksono-4@users.noreply.github.com>
2024-06-28 11:08:55 +0700

    Update import data to mysql.py

--- import data to mysql.py
+++ import data to mysql.py
@@ -7,13 +7,13 @@ df['order_time'] = pd.to_datetime(df['order_time','oreder_date'], format='%H:%M:
 df.drop_duplicates()
 df['order_details_id'].duplicated()
 
-df_pizza_order = df_today.drop(['total_price','pizza_size','pizza_category','pizza_ingredients','pizza_name'], axis=1)
-df_pizza_name = df_today.groupby(['pizza_id','pizza_size'])['pizza_name'].unique().reset_index()
+df_pizza_order = df.drop(['total_price','pizza_size','pizza_category','pizza_ingredients','pizza_name'], axis=1)
+df_pizza_store = df.groupby(['store_id','city'])['store_name'].unique().reset_index()
+df_pizza_store = pd.DataFrame(df_pizza_store, columns=['store_id','city','store_name'])
+df_pizza_name = df.groupby(['pizza_id','pizza_size'])['pizza_name'].unique().reset_index()
 df_pizza_name = pd.DataFrame(df_pizza_name, columns=['pizza_id', 'pizza_size', 'pizza_name'])
-df_pizza = df_today.groupby(['pizza_name','pizza_category'])['pizza_ingredients'].unique().reset_index()
+df_pizza_ingred = df.groupby(['pizza_name','pizza_category'])['pizza_ingredients'].unique().reset_index()
 df_pizza_ingred = pd.DataFrame(df_pizza, columns=['pizza_name','pizza_category','pizza_ingredients'])
-df_pizza_category = df_today.groupby('pizza_name')['pizza_category'].unique().reset_index()
-df_pizza_category = pd.DataFrame(df_pizza_category, columns=['pizza_name','pizza_category'])
 
 conn = mysql.connector.connect(
     host = 'localhost',
@@ -30,12 +30,18 @@ for row in df_pizza_order.itertuples(index = False):
     cursor.execute(insert_query, values)
     conn.commit()
 
+for row in df_pizza_store.itertuples():
+    cursor.execute('''INSERT INTO pizza_store (store_id, city, store_name)
+    values (%s,%s,%s)''', (row.store_id, row.city, row.store_name))
+    conn.commit()
+    
 for row in df_pizza_name.itertuples():
     cursor.execute('''INSERT INTO pizza_name (pizza_id, pizza_size, pizza_name)
     values (%s,%s,%s)''', (row.pizza_id, row.pizza_size, row.pizza_name))
-
+    conn.commit()
+    
 for row in df_pizza_ingred.itertuples():
-    cursor.execute('''INSERT INTO pizza_category (pizza_name, pizza_category, pizza_ingredients)
+    cursor.execute('''INSERT INTO pizza_ingredients (pizza_name, pizza_category, pizza_ingredients)
     values (%s,%s,%s)''', (row.pizza_name, row.pizza_category, row.pizza_ingredients))
     conn.commit()
-    conn.commit()
+    
