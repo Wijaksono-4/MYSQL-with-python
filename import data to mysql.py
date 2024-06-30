e0d1926d1b80722c3ad7f7e7b9cbb17f8491d52f
Wijaksono-4 <73674801+Wijaksono-4@users.noreply.github.com>
2024-06-30 14:56:27 +0700

    Update import data to mysql.py

--- import data to mysql.py
+++ import data to mysql.py
@@ -1,19 +1,19 @@
 import mysql.connector
 import pandas as pd
-from datetime import date
+import numpy as np
 
-df = pd.read_excel(r'C:\Documents\Pizza Sales.xlsx')
-df['order_time'] = pd.to_datetime(df['order_time','oreder_date'], format='%H:%M:%S')
+df = pd.read_excel(r"C:\Users\ACER\OneDrive - mail.unnes.ac.id\Documents\project\folder_to_mysql\target_folder\pizza_sales_st1.csv")
+df['order_details_id'].drop_duplicates()
+df.dropna()
 df.drop_duplicates()
-df['order_details_id'].duplicated()
 
-df_pizza_order = df.drop(['total_price','pizza_size','pizza_category','pizza_ingredients','pizza_name'], axis=1)
+df_pizza_order = df[['order_details_id','order_id','pizza_id','quantity','order_date','order_time','unit_price']]
 df_pizza_store = df.groupby(['store_id','city'])['store_name'].unique().reset_index()
 df_pizza_store = pd.DataFrame(df_pizza_store, columns=['store_id','city','store_name'])
 df_pizza_name = df.groupby(['pizza_id','pizza_size'])['pizza_name'].unique().reset_index()
 df_pizza_name = pd.DataFrame(df_pizza_name, columns=['pizza_id', 'pizza_size', 'pizza_name'])
 df_pizza_ingred = df.groupby(['pizza_name','pizza_category'])['pizza_ingredients'].unique().reset_index()
-df_pizza_ingred = pd.DataFrame(df_pizza, columns=['pizza_name','pizza_category','pizza_ingredients'])
+df_pizza_ingred = pd.DataFrame(df_pizza_ingred, columns=['pizza_name','pizza_category','pizza_ingredients'])
 
 conn = mysql.connector.connect(
     host = 'localhost',
@@ -30,16 +30,19 @@ for row in df_pizza_order.itertuples(index = False):
     cursor.execute(insert_query, values)
     conn.commit()
 
+df_pizza_store['store_id'] = df_pizza_store['store_id'].apply(lambda x: ', '.join(x))
 for row in df_pizza_store.itertuples():
     cursor.execute('''INSERT INTO pizza_store (store_id, city, store_name)
     values (%s,%s,%s)''', (row.store_id, row.city, row.store_name))
     conn.commit()
     
+df_pizza_name['pizza_name'] = df_pizza_name['pizza_name'].apply(lambda x: ', '.join(x))
 for row in df_pizza_name.itertuples():
-    cursor.execute('''INSERT INTO pizza_name (pizza_id, pizza_size, pizza_name)
+    cursor.execute('''INSERT INTO pizza_size_name (pizza_id, pizza_size, pizza_name)
     values (%s,%s,%s)''', (row.pizza_id, row.pizza_size, row.pizza_name))
     conn.commit()
     
+df_pizza_ingred['pizza_ingredients'] = df_pizza_ingred['pizza_ingredients'].apply(lambda x: ', '.join(x))
 for row in df_pizza_ingred.itertuples():
     cursor.execute('''INSERT INTO pizza_ingredients (pizza_name, pizza_category, pizza_ingredients)
     values (%s,%s,%s)''', (row.pizza_name, row.pizza_category, row.pizza_ingredients))
